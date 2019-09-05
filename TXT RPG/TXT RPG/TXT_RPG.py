72a5eeefa14cef7d17e5e56d0300aaac48bb5f09
Highjin <vladonnx@mail.ru>
2019-09-05 23:27:55 +0300

    FLASK

--- TXT RPG/TXT RPG/TXT_RPG.py
+++ TXT RPG/TXT RPG/TXT_RPG.py
@@ -123,6 +123,18 @@ def help_message(message):
    else:
        bot.send_message(message.chat.id,'Чтобы начать играть - напиши /start')
 
+@server.route('/' + config.token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://test-new-new.herokuapp.com/' + config.token)
+    return "!", 200
+
+
 if __name__ == '__main__':
     server.debug = True
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
