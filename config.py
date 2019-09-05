655a0c274c9323cf8bfaf443936f35f044dea58d
Highjin <vladonnx@mail.ru>
2019-09-05 22:36:31 +0300

    REFOLDERING

---
+++ config.py
@@ -0,0 +1,25 @@
+token = '734924842:AAFMvyjdkG2aMDNMKrU-IPnbcp52_lZ260A'
+#assets = r'C:\Users\Acer\Pictures\Saved Pictures'
+#img=open(assets + r'\5Kin.jpg','rb')
+
+
+#параметры состояния
+inWork = False
+isGameStart = False 
+
+
+class proxy1:
+    ip = "69.4.86.195"
+    port = 42063
+
+class proxy2:
+    ip = '178.197.249.251'
+    port = 1080
+
+class char:
+    starv=0
+    sleep=0
+    inte=0
+    stress=0
+
+  
