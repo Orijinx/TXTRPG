b702d7814535c989fab476a26da5db9738dd8e2e
Highjin <vladonnx@mail.ru>
2019-10-01 18:57:50 +0300

    QR CREATING
    Branch will be closed

---
+++ TXT RPG/TXT RPG/gui/ori.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'o.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(320, 240)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.UrlEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.UrlEdit.setObjectName("UrlEdit")
+        self.verticalLayout.addWidget(self.UrlEdit)
+        self.Button = QtWidgets.QPushButton(self.centralwidget)
+        self.Button.setObjectName("Button")
+        self.verticalLayout.addWidget(self.Button)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.Button.setText(_translate("MainWindow", "Generate QR"))
--- TXT RPG/TXT RPG/ori.py
+++ TXT RPG/TXT RPG/ori.py
@@ -1,65 +1,87 @@
-import sys
+import cv2
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication,QDialog)
 import pyqrcode
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QLabel,QFileDialog,QAction,QApplication,QMainWindow)
+from PyQt5.QtGui import QIcon
+import numpy as np
+import argparse
+import qrtools
+from PIL import Image
+from pyzbar.pyzbar import decode
 
+#s = {"firstName": "Иван", "lastName": "Иванов", "address": { "streetAddress": "Московское ш., 101, кв.101", "city": "Ленинград", "postalCode": "101101" }, "phoneNumbers": ["812 123-1234", "916 123-4567"]}
 
-class Ui_Ori(QWidget):
-    def __init__(self):
-        super(Ui_Ori, self).__init__()
-
-    def setupUi(self, Ori):
-        Ori.setObjectName("Ori")
-        Ori.resize(434, 300)
-        self.verticalLayout = QtWidgets.QVBoxLayout(Ori)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("Ori")
+        MainWindow.resize(320, 240)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.lab = QtWidgets.QLabel(Ori)
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setItalic(False)
-        font.setUnderline(False)
-        font.setWeight(75)
-        font.setStrikeOut(False)
-        self.lab.setFont(font)
-        self.lab.setTextFormat(QtCore.Qt.AutoText)
-        self.lab.setObjectName("lab")
-        self.horizontalLayout.addWidget(self.lab)
-        self.URLEdit = QtWidgets.QLineEdit(Ori)
-        self.URLEdit.setObjectName("URLEdit")
-        self.horizontalLayout.addWidget(self.URLEdit)
-        self.verticalLayout.addLayout(self.horizontalLayout)
-        self.gridLayout = QtWidgets.QGridLayout()
-        self.gridLayout.setObjectName("gridLayout")
-        self.CreateButton = QtWidgets.QPushButton(Ori)
-        self.CreateButton.setAutoFillBackground(True)
-        self.CreateButton.setObjectName("CreateButton")
-        self.CreateButton.clicked.connect(self.ButtClicek)
-        self.gridLayout.addWidget(self.CreateButton, 0, 0, 1, 1)
-        self.QRcode = QtWidgets.QGraphicsView(Ori)
-        self.QRcode.setObjectName("QRcode")
-        self.gridLayout.addWidget(self.QRcode, 1, 0, 1, 1)
-        self.verticalLayout.addLayout(self.gridLayout)
-        self.show()
+        self.UrlEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.UrlEdit.setObjectName("UrlEdit")
+        self.verticalLayout.addWidget(self.UrlEdit)
+        self.Button = QtWidgets.QPushButton(self.centralwidget)
+        self.Button.setObjectName("Button")
+        self.Button.clicked.connect(self.ButtClicek)
+        self.verticalLayout.addWidget(self.Button)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        pixmap = QPixmap("code.png")
+        self.lbl = QLabel(self)
+        self.lbl.setPixmap(pixmap)
+        self.verticalLayout.addWidget(self.lbl)
+        #Декодирование QR
+        self.dButton = QtWidgets.QPushButton(self.centralwidget)
+        self.dButton.setObjectName('dButton')
+        self.verticalLayout.addWidget(self.dButton)
+        self.dButton.clicked.connect(self.Decoding)
+        self.Output = QtWidgets.QLineEdit(self.centralwidget)
+        self.Output.setObjectName('OUT')
+        self.verticalLayout.addWidget(self.Output)
 
-        self.retranslateUi(Ori)
-        QtCore.QMetaObject.connectSlotsByName(Ori)
+        
 
-    def retranslateUi(self, Ori):
+
+
+  
+
+
+
+
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Ori.setWindowTitle(_translate("Ori", "Orio"))
-        self.lab.setText(_translate("Ori", "URL"))
-        self.CreateButton.setText(_translate("Ori", "Generate QR"))
+        MainWindow.setWindowTitle(_translate("Ori", "Ori"))
+        self.Button.setText(_translate("Ori", "Generate QR"))
+        self.dButton.setText(_translate("Ori","Decode QR"))
 
     def ButtClicek(self):
-        big_code = pyqrcode.create(self.URLEdit.text, error='L', version=27, mode='binary')
+        URL = self.UrlEdit.text()
+        big_code = pyqrcode.create(URL, error='L', version=5,mode='binary')
         big_code.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
-        big_code.show()
+        pixma = QPixmap("code.png")
+        self.lbl.setPixmap(pixma)
 
-if __name__ == '__main__':
+    def Decoding(self):
+       # d = pyqrcode.decoder
+        try:
+           ### fname = QFileDialog.getOpenFileName(self, 'Open file', '/c')[0]
+           # self.f = open(fname, 'r')
+           self.f = "code.png"
+        except:
+            print('sosi')
+       
 
-    app = QApplication(sys.argv)
-    main = Ui_Ori()
-    sys.exit(app.exec_())
+        #qr = qrtools.QR()
+        qr = decode(Image.open(self.f))
+        print (qr)
+        GT = qr.index
+        self.Output.setText(str(qr))
